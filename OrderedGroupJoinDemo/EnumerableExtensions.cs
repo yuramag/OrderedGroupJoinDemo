af16cb41e414d15040038914bf9a581fb5504b4b
Yuriy Magurdumov <yuriy_magurdumov@mednax.com>
2017-05-03 16:20:32 -0400

    Moving files in

---
+++ OrderedGroupJoinDemo/EnumerableExtensions.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderedGroupJoinDemo
+{
+    public static class EnumerableExtensions
+    {
+        private enum JoinType
+        {
+            Inner,
+            Left,
+            Right,
+            Full
+        }
+
+        private class FilteredEnumerator<T> : IDisposable
+        {
+            private bool m_hasData;
+            private readonly IEnumerator<T> m_enumerator;
+
+            public FilteredEnumerator(IEnumerable<T> sequence)
+            {
+                m_enumerator = sequence.GetEnumerator();
+                m_hasData = m_enumerator.MoveNext();
+            }
+
+            public IEnumerable<T> SkipAndTakeWhile(Predicate<T> filter)
+            {
+                while (m_hasData && !filter(m_enumerator.Current))
+                    m_hasData = m_enumerator.MoveNext();
+
+                while (m_hasData && filter(m_enumerator.Current))
+                {
+                    yield return m_enumerator.Current;
+                    m_hasData = m_enumerator.MoveNext();
+                }
+            }
+
+            public IEnumerable<T> SkipAndTakeWhile(Func<T, int> comparer)
+            {
+                while (m_hasData && comparer(m_enumerator.Current) > 0)
+                    m_hasData = m_enumerator.MoveNext();
+
+                while (m_hasData && comparer(m_enumerator.Current) == 0)
+                {
+                    yield return m_enumerator.Current;
+                    m_hasData = m_enumerator.MoveNext();
+                }
+            }
+
+            public void Dispose()
+            {
+                m_enumerator.Dispose();
+            }
+        }
+
+        private static IEnumerable<TResult> OrderedJoinIterator<TOuter, TInner, TKey, TResult>(
+            this IEnumerable<TOuter> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector,
+            Func<TOuter, TInner, TResult> resultSelector, JoinType jt, IComparer<TKey> comparer)
+        {
+            if (outer == null) throw new ArgumentNullException("outer");
+            if (inner == null) throw new ArgumentNullException("inner");
+            if (outerKeySelector == null) throw new ArgumentNullException("outerKeySelector");
+            if (innerKeySelector == null) throw new ArgumentNullException("innerKeySelector");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+
+            if (comparer == null)
+                comparer = Comparer<TKey>.Default;
+
+            var l = outer.GetEnumerator();
+            var r = inner.GetEnumerator();
+
+            var lHasData = l.MoveNext();
+            var rHasData = r.MoveNext();
+
+            while (lHasData || rHasData)
+            {
+                if (!lHasData)
+                {
+                    if (jt == JoinType.Inner || jt == JoinType.Left)
+                        yield break;
+                    yield return resultSelector(default(TOuter), r.Current);
+                    rHasData = r.MoveNext();
+                    continue;
+                }
+
+                if (!rHasData)
+                {
+                    if (jt == JoinType.Inner || jt == JoinType.Right)
+                        yield break;
+                    yield return resultSelector(l.Current, default(TInner));
+                    lHasData = l.MoveNext();
+                    continue;
+                }
+
+                var comp = comparer.Compare(outerKeySelector(l.Current), innerKeySelector(r.Current));
+
+                if (comp < 0)
+                {
+                    if (jt == JoinType.Left || jt == JoinType.Full)
+                        yield return resultSelector(l.Current, default(TInner));
+                    lHasData = l.MoveNext();
+                }
+                else if (comp > 0)
+                {
+                    if (jt == JoinType.Right || jt == JoinType.Full)
+                        yield return resultSelector(default(TOuter), r.Current);
+                    rHasData = r.MoveNext();
+                }
+                else
+                {
+                    yield return resultSelector(l.Current, r.Current);
+                    lHasData = l.MoveNext();
+                    rHasData = r.MoveNext();
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> OrderedInnerJoin<TOuter, TInner, TKey, TResult>(
+            this IEnumerable<TOuter> left, IEnumerable<TInner> right, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector,
+            Func<TOuter, TInner, TResult> resultSelector, IComparer<TKey> comparer = null)
+        {
+            return OrderedJoinIterator(left, right, outerKeySelector, innerKeySelector, resultSelector, JoinType.Inner, comparer);
+        }
+
+        public static IEnumerable<TResult> OrderedFullJoin<TOuter, TInner, TKey, TResult>(
+            this IEnumerable<TOuter> left, IEnumerable<TInner> right, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector,
+            Func<TOuter, TInner, TResult> resultSelector, IComparer<TKey> comparer = null)
+        {
+            return OrderedJoinIterator(left, right, outerKeySelector, innerKeySelector, resultSelector, JoinType.Full, comparer);
+        }
+
+        public static IEnumerable<TResult> OrderedLeftJoin<TOuter, TInner, TKey, TResult>(
+            this IEnumerable<TOuter> left, IEnumerable<TInner> right, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector,
+            Func<TOuter, TInner, TResult> resultSelector, IComparer<TKey> comparer = null)
+        {
+            return OrderedJoinIterator(left, right, outerKeySelector, innerKeySelector, resultSelector, JoinType.Left, comparer);
+        }
+
+        public static IEnumerable<TResult> OrderedRightJoin<TOuter, TInner, TKey, TResult>(
+            this IEnumerable<TOuter> left, IEnumerable<TInner> right, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector,
+            Func<TOuter, TInner, TResult> resultSelector, IComparer<TKey> comparer = null)
+        {
+            return OrderedJoinIterator(left, right, outerKeySelector, innerKeySelector, resultSelector, JoinType.Right, comparer);
+        }
+
+        public static IEnumerable<TResult> OrderedEqualityGroupJoin<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer,
+            IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector,
+            Func<TOuter, IEnumerable<TInner>, TResult> resultSelector, IEqualityComparer<TKey> comparer = null)
+        {
+            if (outer == null) throw new ArgumentNullException("outer");
+            if (inner == null) throw new ArgumentNullException("inner");
+            if (outerKeySelector == null) throw new ArgumentNullException("outerKeySelector");
+            if (innerKeySelector == null) throw new ArgumentNullException("innerKeySelector");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+
+            if (comparer == null)
+                comparer = EqualityComparer<TKey>.Default;
+
+            var innerEnumerator = new FilteredEnumerator<TInner>(inner);
+            return outer.Select(outerItem => resultSelector(outerItem, innerEnumerator.SkipAndTakeWhile(innerItem => comparer.Equals(outerKeySelector(outerItem), innerKeySelector(innerItem)))));
+        }
+
+        public static IEnumerable<TResult> OrderedCompareGroupJoin<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer,
+            IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector,
+            Func<TOuter, IEnumerable<TInner>, TResult> resultSelector, IComparer<TKey> comparer = null)
+        {
+            if (outer == null) throw new ArgumentNullException("outer");
+            if (inner == null) throw new ArgumentNullException("inner");
+            if (outerKeySelector == null) throw new ArgumentNullException("outerKeySelector");
+            if (innerKeySelector == null) throw new ArgumentNullException("innerKeySelector");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+
+            if (comparer == null)
+                comparer = Comparer<TKey>.Default;
+
+            var innerEnumerator = new FilteredEnumerator<TInner>(inner);
+            return outer.Select(outerItem => resultSelector(outerItem, innerEnumerator.SkipAndTakeWhile(innerItem => comparer.Compare(outerKeySelector(outerItem), innerKeySelector(innerItem)))));
+        }
+    }
+}
---
+++ OrderedGroupJoinDemo/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace OrderedGroupJoinDemo
+{
+    internal class Program
+    {
+        private const int c_count = 10*1000*1000;
+        private const int c_skipCount = 1*1000*1000;
+        private const int c_takeCount = 3;
+
+        private enum DemoId
+        {
+            StandardGroupJoin,
+            OrderedEqualityGroupJoin,
+            OrderedCompareGroupJoin,
+            StandardJoin,
+            OrderedInnerJoin,
+            OrderedFullJoin,
+            OrderedLeftJoin,
+            OrderedRightJoin
+        }
+
+        private static void Main()
+        {
+            var canExit = false;
+            while (!canExit)
+            {
+                Console.WriteLine("Enter Demo ID:");
+                foreach (var demoId in typeof (DemoId).GetEnumValues())
+                    Console.WriteLine("\t{0}. {1}", (int) demoId + 1, demoId);
+                Console.Write("\n|> ");
+                int id;
+                var entry = Console.ReadLine();
+                if (string.IsNullOrEmpty(entry))
+                    canExit = true;
+                else if (int.TryParse(entry, out id))
+                    RunDemo((DemoId) id - 1);
+                Console.WriteLine();
+            }
+        }
+
+        private static void RunDemo(DemoId demoId)
+        {
+            Console.WriteLine("\nRESULTS ({0}):\n", demoId);
+
+            var sw = Stopwatch.StartNew();
+
+            var masterData = GetMasterData(c_count);
+            var detailData = GetDetailData(c_count);
+
+            var outerData = GetMasterData(c_count).Where(x => x.MasterId != c_skipCount + 2);
+            var innerData = GetMasterData(c_count).Where(x => x.MasterId != c_skipCount + 3);
+
+            switch (demoId)
+            {
+                case DemoId.StandardGroupJoin:
+                    masterData.GroupJoin(detailData, m => m.MasterId, d => d.MasterId, Tuple.Create)
+                        .Skip(c_skipCount).Take(c_takeCount).PrintData();
+                    break;
+                case DemoId.OrderedEqualityGroupJoin:
+                    masterData.OrderedEqualityGroupJoin(detailData, m => m.MasterId, d => d.MasterId, Tuple.Create)
+                        .Skip(c_skipCount).Take(c_takeCount).PrintData();
+                    break;
+                case DemoId.OrderedCompareGroupJoin:
+                    masterData.OrderedCompareGroupJoin(detailData, m => m.MasterId, d => d.MasterId, Tuple.Create)
+                        .Skip(c_skipCount).Take(c_takeCount).PrintData();
+                    break;
+                case DemoId.StandardJoin:
+                    outerData.Join(innerData, x => x.MasterId, x => x.MasterId, Tuple.Create)
+                        .Skip(c_skipCount).Take(c_takeCount).PrintData();
+                    break;
+                case DemoId.OrderedInnerJoin:
+                    outerData.OrderedInnerJoin(innerData, x => x.MasterId, x => x.MasterId, Tuple.Create)
+                        .Skip(c_skipCount).Take(c_takeCount).PrintData();
+                    break;
+                case DemoId.OrderedFullJoin:
+                    outerData.OrderedFullJoin(innerData, x => x.MasterId, x => x.MasterId, Tuple.Create)
+                        .Skip(c_skipCount).Take(c_takeCount).PrintData();
+                    break;
+                case DemoId.OrderedLeftJoin:
+                    outerData.OrderedLeftJoin(innerData, x => x.MasterId, x => x.MasterId, Tuple.Create)
+                        .Skip(c_skipCount).Take(c_takeCount).PrintData();
+                    break;
+                case DemoId.OrderedRightJoin:
+                    outerData.OrderedRightJoin(innerData, x => x.MasterId, x => x.MasterId, Tuple.Create)
+                        .Skip(c_skipCount).Take(c_takeCount).PrintData();
+                    break;
+            }
+
+            Console.WriteLine("\nElapsed (ms): {0}", sw.ElapsedMilliseconds);
+        }
+
+        private static IEnumerable<MasterData> GetMasterData(int count)
+        {
+            return Enumerable.Range(1, count).Select(m => new MasterData {MasterId = m});
+        }
+
+        private static IEnumerable<DetailData> GetDetailData(int count)
+        {
+            return Enumerable.Range(1, count).SelectMany(m => Enumerable.Range(1, 5).Select(d => new DetailData {MasterId = m, DetailId = d}));
+        }
+    }
+
+    public static class PrintingExtensions
+    {
+        public static void PrintData(this IEnumerable<Tuple<MasterData, MasterData>> data)
+        {
+            foreach (var masterItem in data)
+                Console.WriteLine("{0} <===> {1}", masterItem.Item1.Data, masterItem.Item2.Data);
+        }
+
+        public static void PrintData(this IEnumerable<Tuple<MasterData, IEnumerable<DetailData>>> data)
+        {
+            foreach (var masterItem in data)
+            {
+                Console.WriteLine(masterItem.Item1.Data);
+                foreach (var detailItem in masterItem.Item2)
+                    Console.WriteLine("\t{0}", detailItem.Data);
+            }
+        } 
+    }
+
+    public struct MasterData
+    {
+        public int MasterId { get; set; }
+
+        public string Data
+        {
+            get { return string.Format("MASTER(Master ID: {0})", MasterId); }
+        }
+    }
+
+    public struct DetailData
+    {
+        public int MasterId { get; set; }
+        public int DetailId { get; set; }
+
+        public string Data
+        {
+            get { return string.Format("DETAIL(Master ID: {0}, Detail ID: {1})", MasterId, DetailId); }
+        }
+    }
+}
